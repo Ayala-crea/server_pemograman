8e602460864e2a897f08a3f6361518a8b7cd9fea
ayalarifki <ayalarifki@gmail.com>
2024-07-01 01:45:21 +0700

    Update authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -4,7 +4,7 @@ const supabase = require("../config/db.js");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs"); // Ensure bcrypt is imported
 const configureMiddleware = require('../middleware/cors.js');
-app.use(configureMiddleware());
+app.use(configureMiddleware);
 
 exports.register = async (req, res) => {
   const { full_name, nickname, email, phone_number, password } = req.body;
